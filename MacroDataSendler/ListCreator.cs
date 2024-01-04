4bcc32930a926139e0e3fb862b354fa150ea1c74
Vovcan <bondavolodiaa@gmail.com>
2024-01-04 18:33:20 +0100

    - create class ListCreator for creating list of macro indicators

---
+++ MacroDataSendler/ListCreator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace MacroDataSendler
+{
+    public class ListCreator
+    {
+
+        private List<(string TickerName, string LinkToTocker)> data;
+        public void ListCrete()
+        {
+            data = new List<(string, string)>();
+            ScrapData();
+        }
+        void ScrapData()
+        {
+            HtmlDocument doc = GetHtml("https://tradingeconomics.com/calendar");
+
+            var eventNodes = doc.DocumentNode.SelectNodes("//td[@style='max-width: 250px; overflow-x: hidden;']");
+
+            if (eventNodes != null)
+            {
+                foreach (var eventNode in eventNodes)
+                {
+                    // Отримати текст та атрибут href з елементу a
+                    string eventName = eventNode.SelectSingleNode(".//a")?.InnerText.Trim();
+                    string eventLink = eventNode.SelectSingleNode(".//a")?.GetAttributeValue("href", "").Trim();
+
+                    // Додати дані до списку
+                    if (!string.IsNullOrEmpty(eventName) && !string.IsNullOrEmpty(eventLink))
+                    {
+                        data.Add((eventName, eventLink));
+                    }
+                }
+            }
+
+            // Вивести дані
+            foreach (var item in data)
+            {
+                Console.WriteLine($"TickerName: {item.TickerName}, LinkToTocker: {item.LinkToTocker}");
+            }
+        }
+
+        private static HtmlDocument GetHtml(string name)
+        {
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument doc = web.Load(name);
+            return doc;
+        }
+    }
+}
--- MacroDataSendler/Program.cs
+++ MacroDataSendler/Program.cs
@@ -1,12 +1,16 @@
 ﻿using MacroDataSendler;
 
-List<string> urls = IndexHistoricalInfo.ReadUrlsFromFile("data.txt");
+//List<string> urls = IndexHistoricalInfo.ReadUrlsFromFile("data.txt");
 
-for (int i = 0; i < urls.Count; i++)
-{
-    var info = IndexHistoricalInfo.GetMarketDataList(urls[i]);
-    foreach (var data in info)
-    {
-        Console.WriteLine($"Name: {data.Name}, Date: {data.Date}, Time: {data.Time}, Month: {data.Month}, Actual: {data.Actual}, Previous: {data.Previous}, Consensus: {data.Consensus}, Forceast: {data.Forecast}");
-    }
-}
+//for (int i = 0; i < urls.Count; i++)
+//{
+//    var info = IndexHistoricalInfo.GetMarketDataList(urls[i]);
+//    foreach (var data in info)
+//    {
+//        Console.WriteLine($"Name: {data.Name}, Date: {data.Date}, Time: {data.Time}, Month: {data.Month}, Actual: {data.Actual}, Previous: {data.Previous}, Consensus: {data.Consensus}, Forceast: {data.Forecast}");
+//    }
+//}
+
+ListCreator tradingeconomistlist = new ListCreator();
+
+tradingeconomistlist.ListCrete();
